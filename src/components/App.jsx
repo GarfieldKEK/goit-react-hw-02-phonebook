218253f8a0329ba4ca294839cbc474a942674640
GarfieldKEK <g0ogles@ukr.net>
2023-06-04 12:37:49 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,138 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { v4 as uuid } from 'uuid'
+import { Component } from 'react';
+import style from "./app.module.css"
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+    name: '',
+    number: '',
+  };
+
+  handleInputChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { name, number } = this.state;
+
+    if (this.isContactExist(name)) {
+      alert('Contact name already exists!');
+      return;
+    }
+
+    const newContact = {
+      id: uuid(),
+      name,
+      number,
+    };
+
+    this.setState((prevState) => ({
+      contacts: [...prevState.contacts, newContact],
+      name: '',
+      number: '',
+    }));
+  };
+
+  isContactExist = (name) => {
+    const { contacts } = this.state;
+    return contacts.some(
+      (contact) => contact.name.toLowerCase() === name.toLowerCase()
+    );
+  };
+
+  handleFilterChange = (e) => {
+    this.setState({ filter: e.target.value });
+  };
+
+  handleDeleteContact = (id) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((contact) => contact.id !== id),
+    }));
+  };
+
+  render() {
+    const { contacts, filter, name, number } = this.state;
+
+    const filteredContacts = contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm
+          name={name}
+          number={number}
+          onInputChange={this.handleInputChange}
+          onSubmit={this.handleSubmit}
+        />
+
+        <h2>Contacts</h2>
+        <Filter filter={filter} onFilterChange={this.handleFilterChange} />
+        <ContactList
+          contacts={filteredContacts}
+          onDeleteContact={this.handleDeleteContact}
+        />
+      </div>
+    );
+  }
+}
+
+const ContactForm = ({ name, number, onInputChange, onSubmit }) => (
+  <form onSubmit={onSubmit}>
+    <input
+    className={style.name}
+      type="text"
+      name="name"
+      pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+      title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+      required
+      value={name}
+      onChange={onInputChange}
+      placeholder="Name"
+    />
+
+    <input
+    className={style.tel}
+      type="tel"
+      name="number"
+      pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+      title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+      required
+      value={number}
+      onChange={onInputChange}
+      placeholder="Phone number"
+    />
+
+    <button className={style.addbtn} type="submit">Add contact</button>
+  </form>
+);
+
+const Filter = ({ filter, onFilterChange }) => (
+  <input
+  className= {style.filter}
+    type="text"
+    name="filter"
+    value={filter}
+    onChange={onFilterChange}
+    placeholder="Search contacts"
+  />
+);
+
+const ContactList = ({ contacts, onDeleteContact }) => (
+  <ul>
+    {contacts.map((contact) => (
+      <li key={contact.id}>
+        {contact.name}: {contact.number}
+        <button className={style.dltbtn} onClick={() => onDeleteContact(contact.id)}>Delete</button>
+      </li>
+    ))}
+  </ul>
+);
