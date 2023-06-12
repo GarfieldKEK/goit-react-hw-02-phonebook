b539bab5455bb0e68a31893f66d28eded5741451
GarfieldKEK <g0ogles@ukr.net>
2023-06-12 11:54:25 +0300

    ))

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,57 +1,15 @@
 import { v4 as uuid } from 'uuid'
 import { Component } from 'react';
-import style from "./app.module.css"
+
 import { ContactForm } from './ContactForm';
 import { Filter } from './Filter';
 import { ContactList } from './ContactList';
+import PropTypes from "prop-types";
 export class App extends Component {
-  static propTypes = {
-    contacts: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        number: PropTypes.string.isRequired,
-      })
-    ),
-    filter: PropTypes.string,
-    name: PropTypes.string,
-    number: PropTypes.string,
-  };
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
 
-  handleInputChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
+  
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const { name, number } = this.state;
 
-    if (this.isContactExist(name)) {
-      alert('Contact name already exists!');
-      return;
-    }
-
-    const newContact = {
-      id: uuid(),
-      name,
-      number,
-    };
-
-    this.setState((prevState) => ({
-      contacts: [...prevState.contacts, newContact],
-      name: '',
-      number: '',
-    }));
-  };
 
   isContactExist = (name) => {
     const { contacts } = this.state;
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,11 +1,19 @@
+
+import PropTypes from 'prop-types';
+;
+
 export const Filter = ({ filter, onFilterChange }) => (
-    <input
-    className= {style.filter}
-      type="text"
-      name="filter"
-      value={filter}
-      onChange={onFilterChange}
-      placeholder="Search contacts"
-    />
-  );
-  
+  <input
+    className={style.filter}
+    type="text"
+    name="filter"
+    value={filter}
+    onChange={onFilterChange}
+    placeholder="Search contacts"
+  />
+);
+
+Filter.propTypes = {
+  filter: PropTypes.string.isRequired,
+  onFilterChange: PropTypes.func.isRequired,
+};
